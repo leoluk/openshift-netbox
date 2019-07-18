526c8240ad62bee699c0ae8ec685b94693aa6373
Tore Anderson <tore@redpill-linpro.com>
2019-07-18 11:41:26 +0200

    Update to NetBox v2.5.13
    Due to digitalocean/netbox@1e1aba73 incorporated in this release, it no longer
    necessary to revert digitalocean/netbox@6f5c35c2 in
    `openshift/s2i/assemble.sh`.

--- openshift/s2i/assemble.sh
+++ openshift/s2i/assemble.sh
@@ -23,34 +23,4 @@ cp /opt/app-root/etc/openshift_urls.py netbox/netbox/openshift_urls.py
 cp /opt/app-root/etc/openshift_middleware.py netbox/netbox/openshift_middleware.py
 cp /opt/app-root/etc/openshift_auth.py netbox/netbox/openshift_auth.py
 
-# Revert commit that breaks 3rd party authentication
-patch -R -p1 <<EOF
-From 6f5c35c2781a1dab157313b1ef87de2ae8de92be Mon Sep 17 00:00:00 2001
-From: Jeremy Stretch <jstretch@digitalocean.com>
-Date: Thu, 28 Feb 2019 11:40:32 -0500
-Subject: [PATCH] Force resolution of request User object when logging an
- object deletion (resolves intermittent test failures)
-
----
- netbox/extras/middleware.py | 6 +++++-
- 1 file changed, 5 insertions(+), 1 deletion(-)
-
-diff --git a/netbox/extras/middleware.py b/netbox/extras/middleware.py
-index 16461c32a..38dde6275 100644
---- a/netbox/extras/middleware.py
-+++ b/netbox/extras/middleware.py
-@@ -29,7 +29,11 @@ def cache_changed_object(instance, **kwargs):
-
- def _record_object_deleted(request, instance, **kwargs):
-
--    # Record that the object was deleted.
-+    # Force resolution of request.user in case it's still a SimpleLazyObject. This seems to happen
-+    # occasionally during tests, but haven't been able to determine why.
-+    assert request.user.is_authenticated
-+
-+    # Record that the object was deleted
-     if hasattr(instance, 'log_change'):
-         instance.log_change(request.user, request.id, OBJECTCHANGE_ACTION_DELETE)
-EOF
-
 fix-permissions /opt/app-root
